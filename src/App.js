fa3ea040c037711325b6e18b5bbd1c8d93ba5507
Elison Moises da Silva Oliveira <ajatosdownload@gmail.com>
2022-06-15 21:58:18 -0300

    adiconando arquivo montando estrutura

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const GridLayout = styled.div`
-height: 100vh;
-width: 100vw;
-display: grid;
-grid-template-areas:"header header header"
-                     "aside main nav"
-                     "aside main nav";
-grid-template-rows: 1fr 4.5fr 5fr;
-grid-template-columns: 1fr 4fr 1fr;
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-areas:
+    "header header header"
+    "aside main nav"
+    "aside main nav";
+  grid-template-rows: 1fr 4.5fr 5fr;
+  grid-template-columns: 1fr 4fr 1fr;
 `;
 
 const Header = styled.header`
-grid-area: header;
-background-color: red;
-`
+  grid-area: header;
+  background-color: red;
+`;
 
 const MenuFiltros = styled.aside`
-grid-area: aside;
-background-color: green;
-`
+  grid-area: aside;
+  background-color: green;
+`;
 
 const MenuCarrinho = styled.nav`
-grid-area: nav;
-background-color: blue;
-`
+  grid-area: nav;
+  background-color: blue;
+`;
 const AreaProdutos = styled.main`
-grid-area: main;
-background-color: orange;
-`
+  grid-area: main;
+  background-color: orange;
+`;
 
 export default class App extends Component {
-
   render() {
-
     return (
       <GridLayout>
-        <Header> </Header> 
+        <Header> </Header>
         <MenuFiltros> </MenuFiltros>
         <MenuCarrinho> </MenuCarrinho>
         <AreaProdutos> </AreaProdutos>
       </GridLayout>
-    )
+    );
   }
 }
-
